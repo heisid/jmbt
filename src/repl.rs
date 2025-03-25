4f727d3e526a094c8d627259b194873a94e5cdbd
Rosyid Haryadi <rosyid_haryadi@protonmail.com>
2025-03-26 03:05:26 +0700

    a

--- src/repl.rs
+++ src/repl.rs
@@ -3,28 +3,31 @@ use std::io::{stdin, stdout, Write};
 pub fn repl() {
     let banner = format!("Jembut {}", env!("CARGO_PKG_VERSION"));
     println!("{}", banner);
-    println!("type 'fuck' to get fucked");
-    println!("type 'exit' or 'quit' to kill yourself");
+    println!("type '.fuck' to get fucked");
+    println!("type '.exit' or '.quit' to kill yourself");
     loop {
         print!("> ");
         stdout().flush().unwrap();
         let mut input = String::new();
-        // let mut output = String::new();
         if let Err(e) = stdin().read_line(&mut input) {
             println!("Error reading input: \n{}", e.to_string());
             std::process::exit(1);
         } else {
-            match input.trim() {
-                "exit" | "quit" => {
-                    std::process::exit(0);
-                },
-                "fuck" => {
-                    println!("fuck you");
-                }
-                _ => {
-                    println!("Unrecognized command");
-                },
+            if let Some(output) = cmd_router(&input.trim()) {
+                println!("{}", output);
             }
         }
     }
+}
+
+fn cmd_router(cmd: &str) -> Option<&str> {
+    let is_meta = cmd.starts_with(".");
+    if is_meta {
+        match cmd {
+            ".exit" | ".quit" => std::process::exit(0),
+            ".fuck" => return Some("fuck you"),
+            _ => return Some("Unrecognized command"),
+        }
+    }
+    Some("kunyuk")
 }
